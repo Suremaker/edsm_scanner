2ae09615bea09748ab7fc7e0ee2880f68e40f7ad
suremaker <wojciech.kotlarski@gmail.com>
2021-05-16 20:33:08 +0100

    Added option to include body data to generated systems file

--- EdsmScanner/Writers/SystemListWriter.cs
+++ EdsmScanner/Writers/SystemListWriter.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 using EdsmScanner.Models;
 
@@ -7,7 +8,7 @@ namespace EdsmScanner.Writers
 {
     internal class SystemListWriter
     {
-        public async Task WriteSystemList(string originSystem, SystemDetails[] systems, bool journeyPlotted)
+        public async Task WriteSystemList(string originSystem, SystemDetails[] systems, bool includeBodies, bool journeyPlotted)
         {
             var path = PathSanitizer.SanitizePath($"systems_{originSystem}.txt");
             await using var writer = new StreamWriter(path);
@@ -17,10 +18,32 @@ namespace EdsmScanner.Writers
             foreach (var sys in systems)
             {
                 var distance = journeyPlotted ? sys.PlottedDistance : sys.Ref.Distance;
-                await writer.WriteLineAsync($"{sys.Ref.Name} [{distance:F2}ly] ({sys.Ref.BodyCount?.ToString() ?? "?"} bodies / {sys.Bodies?.Length.ToString() ?? "?"} discovered) => {sys.Url}");
+                await WriteSystem(writer, sys, distance);
+                if (includeBodies)
+                {
+                    foreach (var body in sys.Bodies.OrderBy(b => b.DistanceToArrival))
+                    {
+                        await WriteBody(writer, body);
+                    }
+                }
             }
 
             Console.WriteLine($"Generated {path} with {systems.Length} systems.");
+        }
+
+        private static async Task WriteSystem(StreamWriter writer, SystemDetails sys, decimal distance)
+        {
+            await writer.WriteLineAsync($"{sys.Ref.Name} [{distance:F2}ly] ({sys.Ref.BodyCount?.ToString() ?? "?"} bodies / {sys.Bodies?.Length.ToString() ?? "?"} discovered / {sys.StarCount} stars discovered) => {sys.Url}");
+        }
+
+        private static async Task WriteBody(StreamWriter writer, SystemBody body)
+        {
+            if (body.Type.Equals("star", StringComparison.OrdinalIgnoreCase))
+                await writer.WriteLineAsync($"  {body.Name}: {body.Type}/{body.SubType} distance:{body.DistanceToArrival}ls {(body.IsScoopable.GetValueOrDefault() ? "scoopable" : "non-scoopable")} age:{body.Age} luminosity:{body.Luminosity} spectralClass:{body.SpectralClass} mass:{body.SolarMasses} radius:{body.SolarRadius} temperature:{body.SurfaceTemperature}K reserve:{body.ReserveLevel} belts:{string.Join(",", body.Belts.Select(b => b.Type))} rings:{string.Join(",", body.Rings.Select(b => b.Type))}");
+            else if (body.Type.Equals("planet", StringComparison.OrdinalIgnoreCase))
+                await writer.WriteLineAsync($"  {body.Name}: {body.Type}/{body.SubType} distance:{body.DistanceToArrival}ls {(body.IsLandable.GetValueOrDefault() ? "landable" : "not-landable")} gravity:{body.Gravity}G earthMasses:{body.EarthMasses} temperature:{body.SurfaceTemperature}K pressure:{body.SurfacePressure} atmosphere:{body.AtmosphereType} volcanism:{body.VolcanismType} terraforming:{body.TerraformingState} reserve:{body.ReserveLevel} rings:{string.Join(",", body.Rings.Select(b => b.Type))}");
+            else
+                await writer.WriteLineAsync($"  {body.Name}: {body.Type}/{body.SubType} distance:{body.DistanceToArrival}ls");
         }
     }
 }
